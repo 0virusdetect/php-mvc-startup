763ebde69aa609ac462a4817fb7adda769ffd4b8
Abhihsek Jaiswal <47599266+0virusdetect@users.noreply.github.com>
2024-07-07 07:58:34 +0530

    Delete app directory

--- app/views/user/display/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include '../app/views/user/layout/header.php';
-?>
-<p>display</p>
-<?php 
-include '../app/views/user/layout/footer.php';
-?>
--- app/views/user/layout/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>footer</p>
-</footer>
-<script src="<?php echo $public;?>js/script.js"></script>
-</body>
-</html>
