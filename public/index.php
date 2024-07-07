bc0667d1d5aded5c286928b6825945d76fe82e83
Abhihsek Jaiswal <47599266+0virusdetect@users.noreply.github.com>
2024-07-07 07:27:09 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+
+$host = $_SERVER['HTTP_HOST'];
+if (strpos($host, 'localhost:') !== false) {
+$public='';
+} else {
+$public='public/';
+}
+
 require_once '../config/routes.php';
 ?>
